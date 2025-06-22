64914c622e5f1bf489b0157c18640995d5064a73
iamDhruvAgarwal <dhruvindiajaipur@gmail.com>
2025-06-22 09:06:31 +0530

    nhi hua ye abhi 3 problems wala code

---
+++ A_Three_Problems.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //we have to choose 3 problems and we are given N problems to choose from
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    unordered_map <int, int> hash;
+    
+    
+    return 0;
+}
